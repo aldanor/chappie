961bff5d82a27b771c286850e66ff7300a4921cd
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-09-06 02:58:53 +0100

    Further refactoring and simplification; introduce search goals; dfs is a method

--- src/search.rs
+++ src/search.rs
@@ -3,15 +3,6 @@ use std::collections::HashSet;
 use std::marker::PhantomData;
 use std::hash::{Hash, Hasher, SipHasher};
 
-pub trait SearchSpace {
-    type State: Hash;
-    type Action;
-    type Iterator: Iterator<Item=(Self::Action, Self::State)>;
-
-    fn expand(&self, state: &Self::State) -> Self::Iterator;
-    fn is_goal(&self, state: &Self::State) -> bool;
-}
-
 struct HashTracker<T> {
     hash_set: HashSet<u64>,
     phantom: PhantomData<T>,
@@ -37,104 +28,108 @@ impl<T> HashTracker<T> where T: Hash {
     }
 }
 
-pub fn depth_first_search<S>(space: &S, start: S::State) -> Option<Vec<S::Action>>
-where S: SearchSpace {
-    if space.is_goal(&start) {
-        return Some(vec![]);
+pub trait SearchGoal<T> {
+    fn is_goal(&self, state: &T) -> bool;
+}
+
+impl<T> SearchGoal<T> for T where T: PartialEq {
+    fn is_goal(&self, state: &T) -> bool {
+        *self == *state
     }
+}
 
-    let mut path = Vec::new();
-    let mut visited = HashTracker::new();
-    let mut frontier = vec![space.expand(&start)];
+pub trait SearchSpace {
+    type State: Hash;
+    type Action;
+    type Iterator: Iterator<Item=(Self::Action, Self::State)>;
 
-    loop {
-        let result = match frontier.last_mut() {
-            None => {
-                return None
-            },
-            Some(&mut ref mut iter) => {
-                match iter.next() {
-                    None => {
-                        path.pop();
-                        None
-                    },
-                    Some((action, state)) => {
-                        if visited.contains(&state) {
-                            continue;
+    fn expand(&self, state: &Self::State) -> Self::Iterator;
+
+    fn dfs<G>(&self, start: Self::State, goal: G) -> Option<Vec<Self::Action>>
+    where G: SearchGoal<Self::State> {
+        if goal.is_goal(&start) {
+            return Some(vec![]);
+        }
+
+        let mut path = Vec::new();
+        let mut visited = HashTracker::new();
+        let mut frontier = vec![self.expand(&start)];
+
+        loop {
+            let result = match frontier.last_mut() {
+                None => {
+                    return None
+                },
+                Some(&mut ref mut iter) => {
+                    match iter.next() {
+                        None => {
+                            path.pop();
+                            None
+                        },
+                        Some((action, state)) => {
+                            if visited.contains(&state) {
+                                continue;
+                            }
+                            path.push(action);
+                            if goal.is_goal(&state) {
+                                return Some(path);
+                            }
+                            Some(self.expand(&state))
                         }
-                        path.push(action);
-                        if space.is_goal(&state) {
-                            return Some(path);
-                        }
-                        Some(space.expand(&state))
                     }
                 }
-            }
-        };
-        match result {
-            None => {
-                frontier.pop();
-            }
-            Some(iter) => {
-                frontier.push(iter);
-            }
-        };
+            };
+            match result {
+                None => {
+                    frontier.pop();
+                }
+                Some(iter) => {
+                    frontier.push(iter);
+                }
+            };
+        }
     }
 }
 
 #[cfg(test)]
 pub mod tests {
     use std::vec::IntoIter;
-    use super::*;
+    use super::SearchSpace;
 
     #[test]
-    pub fn test_depth_first_search() {
-        struct S {
-            goal: i32,
-        }
+    pub fn test_dfs() {
+        struct TestSearch;
 
         #[derive(Debug, PartialEq)]
-        enum Direction {
-            Left,
-            Right
-        }
+        enum Dir { Left, Right }
 
-        impl SearchSpace for S {
+        impl SearchSpace for TestSearch {
             type State = i32;
-            type Action = Direction;
+            type Action = Dir;
             type Iterator = IntoIter<(Self::Action, Self::State)>;
 
             fn expand(&self, state: &Self::State) -> Self::Iterator {
                 if *state == 0 {
-                    vec![(Direction::Left, 1), (Direction::Right, 2)].into_iter()
+                    vec![(Dir::Left, 1), (Dir::Right, 2)].into_iter()
                 } else if *state == 1 {
-                    vec![(Direction::Left, 3), (Direction::Right, 4)].into_iter()
+                    vec![(Dir::Left, 3), (Dir::Right, 4)].into_iter()
                 } else if *state == 2 {
-                    vec![(Direction::Left, 2)].into_iter()
+                    vec![(Dir::Left, 2)].into_iter()
                 } else {
                     vec![].into_iter()
                 }
             }
-
-            fn is_goal(&self, state: &i32) -> bool {
-                return self.goal == *state
-            }
         }
 
-        impl S {
-            fn solve(start: i32, goal: i32) -> Option<Vec<Direction>> {
-                let space = S { goal: goal };
-                depth_first_search(&space, start)
-            }
-        }
+        let ts = TestSearch;
 
-        assert_eq!(S::solve(0, 0).unwrap(), vec![]);
-        assert_eq!(S::solve(0, 1).unwrap(), vec![Direction::Left]);
-        assert_eq!(S::solve(0, 2).unwrap(), vec![Direction::Right]);
-        assert_eq!(S::solve(0, 3).unwrap(), vec![Direction::Left, Direction::Left]);
-        assert_eq!(S::solve(0, 4).unwrap(), vec![Direction::Left, Direction::Right]);
-        assert_eq!(S::solve(2, 2).unwrap(), vec![]);
-        assert!(S::solve(2, 0).is_none());
-        assert!(S::solve(5, 0).is_none());
+        assert_eq!(ts.dfs(0, 0).unwrap(), vec![]);
+        assert_eq!(ts.dfs(0, 1).unwrap(), vec![Dir::Left]);
+        assert_eq!(ts.dfs(0, 2).unwrap(), vec![Dir::Right]);
+        assert_eq!(ts.dfs(0, 3).unwrap(), vec![Dir::Left, Dir::Left]);
+        assert_eq!(ts.dfs(0, 4).unwrap(), vec![Dir::Left, Dir::Right]);
+        assert_eq!(ts.dfs(2, 2).unwrap(), vec![]);
+        assert!(ts.dfs(2, 0).is_none());
+        assert!(ts.dfs(5, 0).is_none());
     }
 }
